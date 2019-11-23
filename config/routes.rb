1af17b8db70704a547438b2c9d3b72cb5efaf9a4
Subhan Mahmood <subhan70m@gmail.com>
2019-11-23 19:12:49 +0000

    added home and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#home'
+  get 'contact', to: 'home#contact'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
